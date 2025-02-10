b549bc9a54e11da836d930f9274e9570e7b53385
Sketox <esquetinis007@gmail.com>
2025-02-10 15:35:40 -0500

    Creadas las clases 'Paciente' y 'Citas' para todas las carpetas correspondiente

--- src/main/java/ec/webmarket/restful/persistence/PacienteRepository.java
+++ src/main/java/ec/webmarket/restful/persistence/PacienteRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ec.webmarket.restful.domain.Paciente;
 
-public interface ClienteRepository extends JpaRepository<Paciente, Long> {
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
 	List<Paciente> findByNombre(String nombre);
 
