9fd9238b8613da576b6b8c02a7f99b87d7d335eb
Sketox <esquetinis007@gmail.com>
2025-02-10 16:05:17 -0500

    Subido correctamente los acrchivos de 'citas'

---
+++ src/main/java/ec/webmarket/restful/service/crud/CitaService.java
@@ -0,0 +1,48 @@
+package ec.webmarket.restful.service.crud;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ec.webmarket.restful.domain.Paciente;
+import ec.webmarket.restful.dto.v1.PacienteDTO;
+import ec.webmarket.restful.persistence.PacienteRepository;
+import ec.webmarket.restful.service.GenericCrudServiceImpl;
+
+@Service
+public class CitaService extends GenericCrudServiceImpl<Paciente, PacienteDTO> {
+
+	@Autowired
+	private PacienteRepository repository;
+
+	private ModelMapper modelMapper = new ModelMapper();
+
+	@Override
+	public Optional<Paciente> find(PacienteDTO dto) {
+		return repository.findById(dto.getId_cliente());
+	}
+
+	@Override
+	public void delete(PacienteDTO dto) {
+		repository.deleteById(dto.getId_cliente());
+	}
+	
+	@Override
+	public Paciente mapToDomain(PacienteDTO dto) {
+		return modelMapper.map(dto, Paciente.class);
+	}
+
+	@Override
+	public PacienteDTO mapToDto(Paciente domain) {
+		return modelMapper.map(domain, PacienteDTO.class);
+	}
+	
+	public Optional<Paciente> findByCedula(Long cedula) {
+	    return repository.findByCedula(cedula);
+	}
+
+}
